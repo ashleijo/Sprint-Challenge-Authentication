09df0744f6f44f5dc6fbe7ebba58d569a3a4fe1e
ashleijo <shuryjo@gmail.com>
2018-03-23 12:24:00 -0400

    initial commit, able to post new user & return token upon login

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,17 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  if (!username || !password) {
+    sendUserError('You will need to enter a username & password.', res);
+  }
+  const user = new User({ username, password });
+  user.save()
+  .then((newUser) => {
+    res.status(200).json(newUser);
+  })
+  .catch((err) => {
+    res.status(500).json({ error: 'There was a server error while signing up', err });
+  });
 };
 
 module.exports = {
